2fb5bf2e8e1cfb67c68107199b0cb82a960f1bab
Francisco Nunes <fnfneto@gmail.com>
2023-11-11 16:15:16 -0300

    atualizacao

--- src/ferreira/cnt/estrutura/ListaDeTelefoneNaoOrdenado.java
+++ src/ferreira/cnt/estrutura/ListaDeTelefoneNaoOrdenado.java
@@ -11,5 +11,26 @@ public class ListaDeTelefoneNaoOrdenado {
 		this.lstTelefones = new Telefone[CAPACIDADE_PADRAO];
 		this.tamanho = 0;
 	}
+	
+	public boolean isEmpty() {
+		return tamanho == 0;
+	}
+	
+	public boolean isFull() {
+		return tamanho == lstTelefones.length;
+	}
+	
+	public void removerTelefone(Telefone lstTelefones) {
+		int index = //buscarTelefoneIndex(Telefone)
+	}
+	
+	private int buscarTelefoneIndex(Telefone Telefone) {
+		for(int i = 0; i < tamanho; i++) {
+			if(lstTelefones[i]. equals(telefone)) {
+				return i;
+			}
+		}
+	}
+				
 
 }
